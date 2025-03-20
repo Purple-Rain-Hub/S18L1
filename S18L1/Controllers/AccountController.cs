236e978c016d652582391847343da17c9e25ad0e
Manuel Palmieri <manuelpalm2002@gmail.com>
2025-03-20 14:20:08 +0100

    aggiunto Login, Logout, ruolo Studente

--- S18L1/Controllers/AccountController.cs
+++ S18L1/Controllers/AccountController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using S18L1.Models;
 using S18L1.ViewModels;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 
 namespace S18L1.Controllers
 {
@@ -18,8 +22,18 @@ namespace S18L1.Controllers
             _roleManager = roleManager;
         }
 
+        private bool VerifyAuth()
+        {
+            return User.Identity.IsAuthenticated;
+        }
+
         public IActionResult RegisterPage()
         {
+            if (VerifyAuth())
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
             return View();
         }
 
@@ -36,6 +50,74 @@ namespace S18L1.Controllers
 
             var result = await _userManager.CreateAsync(newUser, model.Password);
 
+            if (!result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var user = await _userManager.FindByEmailAsync(newUser.Email);
+
+            if(user == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            await _userManager.AddToRoleAsync(user, "Student");
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        public IActionResult LoginPage()
+        {
+            if (VerifyAuth())
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("LoginPage");
+            }
+
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user == null)
+            {
+                return RedirectToAction("LoginPage");
+            }
+
+            var signinResult = await _signInManager.PasswordSignInAsync(user, model.Password, true, false);
+            if (!signinResult.Succeeded)
+            {
+                return RedirectToAction("LoginPage");
+            }
+
+            var claims = new List<Claim>();
+
+            var nameClaim = new Claim(ClaimTypes.Name, $"{user.FirstName} {user.LastName}");
+
+            var emailClaim = new Claim(ClaimTypes.Email, user.Email);
+
+            claims.Add(nameClaim);
+            claims.Add(emailClaim);
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+        
+            return RedirectToAction("Index", "Home");
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             return RedirectToAction("Index", "Home");
         }
     }
---
+++ S18L1/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace S18L1.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [PasswordPropertyText]
+        [Required]
+        public required string Password { get; set; }
+    }
+}
